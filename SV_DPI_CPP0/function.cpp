b77383e06e07e2a727da2847436c9d1158c22f30
Hossam Hassan <hossamfadeel@users.noreply.github.com>
2022-03-28 17:17:58 +0900

    Create function.cpp

---
+++ SV_DPI_CPP0/function.cpp
@@ -0,0 +1,14 @@
+
+#include "svdpi.h"
+
+//----------------------------------------------
+// C++ file
+//----------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+ 
+extern "C" void c_method() {
+ 
+  printf("     Hello World...!\n");
+ 
+}
